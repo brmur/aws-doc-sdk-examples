a6438dc724837e79e7c532f14eaac3246b5fe61b
Brian Murray <40031786+brmur@users.noreply.github.com>
2025-05-12 11:48:07 +0100

    add experimental prompt files

--- javav2/example_code/bedrock/src/main/java/com/example/bedrock/sync/testfile.java
+++ javav2/example_code/bedrock/src/main/java/com/example/bedrock/sync/testfile.java
@@ -1,5 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 // snippet-start:[bedrock.testfile]
-
 // snippet-end:[bedrock.testfile]
